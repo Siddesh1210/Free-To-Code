8e4fa942edadf3d67694574ef1e43d9c6a7ab6b5
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-01-02 10:29:57 +0530

    tc of 02/01/2023

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -13,7 +13,7 @@ function Hero()
                 <div className="row front-page justify-content-evenly">
                 <div className="col-md-7 col-lg-9 text-center front-div" data-aos="fade-down" data-aos-delay="50" data-aos-duration="2000" data-aos-easing="ease-in-out">
                 <h1>Become a Certified <span className="inside-front-font">Front End Developer</span> in 30 Days At Just Rs  499/- <del className="text-secondary fw-lighter">2999</del></h1>
-                <p className="text-white">100% Money Back Guarantee*</p>
+                <p className="text-white">100% Money Back Guarantee*<br></br><span className='razorpay-text'>Batch starting : 9th January 2023</span></p>
                 <button className="register-btn" onClick={submitHandler} data-aos="flip-up" data-aos-delay="1500" data-aos-duration="2000" data-aos-easing="ease-in-out">Enroll Now</button>
                 </div>
                 </div>
